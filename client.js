d90386774aa178b8e0ae753df0d3a2038ab23a61
Al Camara <Alhousseinec@gmail.com>
2022-08-27 19:14:42 -0500

    finish jquery effect

--- client.js
+++ client.js
@@ -1,5 +1,47 @@
 $(document).ready(readyNow);
 
 function readyNow() {
+    const boxes = document.querySelectorAll('.box');
+
     console.log('test');
+
+    // $(document).on('scroll',checkBoxes)
+
+    $(document).on('scroll',()=>{
+
+        const triggerBottom = window.innerHeight / 5*4;
+        console.log(triggerBottom);
+
+        boxes.forEach(box =>{
+            const boxTop = box.getBoundingClientRect().top;
+
+            if (boxTop < triggerBottom) {
+                box.classList.add('show')
+            } else {
+                box.classList.remove('show');
+            }
+
+        })
+        
+    });
+
+
 }
+
+// function checkBoxes() {
+//     const triggerBottom = window.innerHeight / 5*4;
+//          console.log(triggerBottom);
+
+//          const boxes = $('.container').children()
+
+//          boxes.each((box)=>{
+//             const boxTop = box.getBoundingClientRect().top;
+
+//             if (boxTop < triggerBottom) {
+//                 box.classList.add('show')
+//             } else {
+//                 box.classList.remove('show');
+//             }
+
+//          })
+// }
